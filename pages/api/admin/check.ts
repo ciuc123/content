69e981222f92be7643f86274cd9c31e0a3315115
Andrei Ciuculescu <andrei@ciuculescu.com>
2026-07-13 17:22:31 +0300

    - add version

--- pages/api/admin/check.ts
+++ pages/api/admin/check.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+  import type { NextApiRequest, NextApiResponse } from 'next'
 import { withAdminAuth } from '../../../lib/clerk'
 import { supabaseServer } from '../../../lib/supabase'
 
